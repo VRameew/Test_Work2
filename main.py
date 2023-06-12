30d060990115e1dbe904d6e3e87a33896a7cae23
Vlad-Work <v.rameew@gmail.com>
2023-06-12 11:23:39 +0300

    Create Users API

--- main.py
+++ main.py
@@ -2,19 +2,30 @@ import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 import psycopg2
 from sqlalchemy.ext.declarative import declarative_base
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import uuid
 
 #Data class for SQL base
 Base = declarative_base()
 
 
-class Users(Base):
+class User(Base):
+    __tablename__ = "Users"
+    
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.Text, nullable=False)
+    token = sa.Column(sa.Text, nullable=False)
 
 
-class Records(Base):
+class Record(Base):
+    __tablename__ = "Records"
+    
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    user_id = sa.Column(sa.Text, nullable=False)
+    url = sa.Column(sa.Text, nullable=False)
 
 #Creating SQL Base
-engine = sa.create_engine("postgresql+psycopg2://postgres:postgres@DB", echo=True, pool_pre_ping=True)
+engine = sa.create_engine("postgresql+psycopg2://postgres:postgres@localhost/DB", echo=True, pool_pre_ping=True)
 #Creating tables
 Base.metadata.create_all(engine)
 
@@ -26,3 +37,19 @@ session = DBSession()
 
 #Start FastAPI APP
 app = FastAPI()
+#Finding users by token
+def get_user(token: str):
+    users = session.query(User).all()
+    for user in users:
+        if user.token == token:
+            return user
+    raise HTTPException(status_code=401, detail="Invalid token")
+    
+@app.post("/users", response_model=User)
+def user_creating("/users", response_model=User):
+    id = str(uuid.uuid4())
+    token = str(uuid.uuid4())
+    user = User(id=id, name=name, token=token)
+    session.add(user)
+    session.commit()
+    return user
